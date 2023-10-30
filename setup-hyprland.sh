1b6939b5860e7766a83e262bb4a400a40f7379d8
bozidarsk <bozidarkabahcijski@gmail.com>
2023-10-30 19:57:20 +0200

    Fixed bugs.

--- setup-hyprland.sh
+++ setup-hyprland.sh
@@ -21,4 +21,8 @@ chmod +x /tmp/gsettings.sh
 sudo pacman -Syu jdk-openjdk libxtst fontconfig freetype2 lib32-gcc-libs lib32-glibc libx11 libxext libxrender zlib
 installaur thorium-browser-bin spotify hyprpicker-git swaync pulsemeeter-git sublime-text-4 unityhub android-sdk celluloid-git wifi-menu-git #media-menu-git sound-menu-git power-menu-git cliphistory-git
 
+echo "export GTK_THEME=Colloid-Dark-Nord" >> ~/.zshenv
+echo "export ICON_THEME=Colloid-teal-nord" >> ~/.zshenv
+echo "export CURSOR_THEME=Win-8.0-NS" >> ~/.zshenv
+
 sudo rm -rf $dotfiles
