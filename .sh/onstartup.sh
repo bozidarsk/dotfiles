c2e5977ebf40d46972c7ec4f581094c4c46912ba
bozidarsk <100208962+bozidarsk@users.noreply.github.com>
2023-10-31 15:35:26 +0200

    Fixed bugs.

--- .sh/onstartup.sh
+++ .sh/onstartup.sh
@@ -4,7 +4,7 @@ swaync & disown
 playerctld daemon & disown
 wl-paste --type text/plain --watch cliphistory store text & disown
 wl-paste --type image/png --watch cliphistoty store image & disown
-rclone mount "Google Drive:" "/mnt/external/Google Drive" & disown
+rclone mount "Google Drive:" "$HOME/Google Drive" & disown
 hyprctl setcursor "$CURSOR_THEME" 24 & disown
 libinput debug-events --show-keycodes | media-menu window & disown
 
