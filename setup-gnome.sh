7b37ef0e9c5f041e775e2fa4b21144c212fd3c26
bozidarsk <bozidarsk@proton.me>
2023-11-01 12:42:04 +0200

    Added libinput-gestures.

--- setup-gnome.sh
+++ setup-gnome.sh
@@ -8,14 +8,18 @@ sudo systemctl disable iwd
 
 mv $dotfiles/.config/pulsemeeter .config/
 mv $dotfiles/.config/sublime-text/Packages .config/sublime-text/
+mv $dotfiles/.config/libinput-gestures.conf .config/libinput-gestures.conf
 
 cat $dotfiles/gsettings/gnome | sed -E 's/(.+)/gsettings set \1/' > /tmp/gsettings.sh
 chmod +x /tmp/gsettings.sh
 /tmp/gsettings.sh 
 
-installaur gnome-browser-connector
+installaur gnome-browser-connector libinput-gestures
+
+libinput-gestures-setup autostart start
 
 echo "Gtk theme: Colloid-Dark-Nord"
 echo "Cursro theme: Nordic-cursors"
 
 sudo rm -rf $dotfiles
+sudo rm /usr/local/bin/onstartup
