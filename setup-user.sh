e34f6eb0fdaebe9e3bbda3e00f3761273569f9fe
bozidarsk <bozidarsk@proton.me>
2026-03-28 14:34:29 +0200

    Added missing file for chmod.

--- setup-user.sh
+++ setup-user.sh
@@ -20,9 +20,9 @@ sudo ufw allow 443/tcp
 sudo ufw allow from $printerip
 sudo ufw allow to $printerip
 
-sudo chmod 777
+sudo chmod 777 /etc/fstab
 echo "UUID=8C346B12346AFE98 /mnt/external ntfs defaults,uid=$UID,gid=$GID 0 2" >> /etc/fstab
-sudo chmod 644
+sudo chmod 644 /etc/fstab
 
 sudo timedatectl set-timezone "$timezone"
 sudo timedatectl set-ntp true
