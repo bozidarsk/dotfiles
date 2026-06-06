aa9093fc966171c85c6ae255a7cedc920cea370c
bozidarsk <bozidarsk@proton.me>
2026-06-06 12:11:49 +0300

    Added more packages.

--- setup-root.sh
+++ setup-root.sh
@@ -19,7 +19,7 @@ pacman -Syy \
     sudo man-db man-pages base-devel dinit-user-spawn glib2-devel qt6-wayland libadwaita gtk4 ttf-liberation \
     ntfs-3g dosfstools exfatprogs mtools libisoburn android-udev android-file-transfer \
     openssh openssh-dinit ntp ntp-dinit ufw ufw-dinit vim git zsh wget less tree zip unzip unrar gnu-netcat strace tmux \
-    zed dotnet-sdk mono nasm gtk-sharp-3 gtk-layer-shell rust docker docker-compose docker-dinit arduino-cli \
+    zed dotnet-sdk mono nasm gtk-sharp-3 gtk-layer-shell rust docker docker-compose docker-dinit arduino-cli gnu-efi \
     qemu-full qemu-ui-gtk edk2-ovmf swtpm virt-viewer \
     cups cups-dinit cups-pdf sane sane-dinit avahi avahi-dinit \
     playerctl pipewire pipewire-dinit pipewire-pulse pipewire-pulse-dinit wireplumber wireplumber-dinit jack2 gstreamer gst-plugin-pipewire mpv x265 x264 ffmpegthumbnailer \
