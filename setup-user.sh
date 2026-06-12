c00c47b7dd4bea26af56017a778154c35d278cb0
bozidarsk <bozidarsk@proton.me>
2026-06-12 15:19:57 +0300

    Updated fstab.

--- setup-user.sh
+++ setup-user.sh
@@ -26,7 +26,7 @@ sudo ufw allow 443/tcp
 sudo ufw allow from $printerip
 sudo ufw allow to $printerip
 
-echo "UUID=8C346B12346AFE98 /mnt/external ntfs defaults,uid=$UID,gid=$GID 0 2" | sudo tee -a /etc/fstab
+echo "UUID=8C346B12346AFE98 /mnt/external ntfs-3g defaults,uid=$UID,gid=$GID,noatime 0 0" | sudo tee -a /etc/fstab
 
 sudo ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 sudo hwclock --systohc
